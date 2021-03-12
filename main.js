f419816871a23d31af5729c4bf52f2cd127c1d2b
GhazaryanS <ghazaryans96@gmail.com>
2021-03-13 01:23:25 +0400

    add or delete

---
+++ main.js
@@ -0,0 +1,29 @@
+const board = document.querySelector('#board');
+const addBtn = document.querySelector('#add-cub');
+const deleteBtn = document.querySelector('#delete');
+const randomColor = () => Math.floor(Math.random() * 255);
+let userStatus = undefined;
+
+addBtn.onclick = () => {
+    userStatus = 'add';
+    board.style.cursor = 'crosshair';
+};
+
+deleteBtn.onclick = () =>{
+     userStatus = 'delete';
+     board.style.cursor = 'not-allowed';
+}
+
+board.onclick = event => {
+    if(userStatus === 'add') {
+        let xCoordinate = event.layerX - 15;
+        let yCoordinate = event.layerY - 15;
+        if(xCoordinate <16 || yCoordinate < 16){
+            return alert(`Will be outside the area or a Cub has already been created there.
+            Try clicking elsewhere!!!`);
+        };
+        board.innerHTML += `<div class="canvas__cub" style="top: ${yCoordinate}px; left: ${xCoordinate}px; background-color: rgb(${randomColor()}, ${randomColor()}, ${randomColor()})"></div>`;
+    } else if(userStatus === 'delete') {
+        document.querySelectorAll(".canvas__cub").forEach(element => element.onclick = () => element.remove());
+    };
+};
